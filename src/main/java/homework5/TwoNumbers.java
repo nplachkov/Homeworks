1a12e235bf29658e8b7e28742fbe9024eb32228c
nplachkov <nikito12386@gmail.com>
2023-09-06 14:51:36 +0300

    Update

--- src/main/java/homework5/TwoNumbers.java
+++ src/main/java/homework5/TwoNumbers.java
@@ -7,12 +7,18 @@ public class TwoNumbers {
         double number3 = 15;
         double number4 = 20;
 
-        twoAverage(number4,number2);
+        twoAverage(number1,number2);
 
 
     }
     public static void twoAverage(double numberOne, double numberTwo){
         double average = (numberOne + numberTwo)/2;
-        System.out.println(average);
+        System.out.println("The average of those numbers is: " + average);
+        sumTwoNumbers(numberOne, numberTwo);
+    }
+
+    public static void sumTwoNumbers(double numberOne, double numberTwo){
+        double summedNumbers = numberOne + numberTwo;
+        System.out.println("The sum of the numbers is " + summedNumbers);
     }
 }
