17ca110d060beb856bea05b33829281afb7904da
nplachkov <nikito12386@gmail.com>
2023-10-13 11:36:42 +0300

    Updated the password validation.

--- src/main/java/homework5/PasswordValidation.java
+++ src/main/java/homework5/PasswordValidation.java
@@ -7,41 +7,18 @@ public class PasswordValidation {
         Scanner scanner = new Scanner(System.in); //Scanner
 
         System.out.printf("Enter password: "); //Asks user for password
-        String password = scanner.nextLine(); //Stores the user input in a string
+        String passwordInput = scanner.nextLine(); //Stores the user input in a string
 
-        isValidPassword(password);
-
+        if (isValidPassword(passwordInput) == true){ //Prints out a text whether the password is secure enough or not
+            System.out.println("Password secure.");
+        }
+        else {
+            System.out.println("Password not secure enough.");
+        }
     }
-    public static boolean isValidPassword(String password){ //Validates the provided String based of the below specifications.
-        boolean isValid = true;
-        String upperCaseChars = "(.*[A-Z].*)";
-        String lowerCaseChars = "(.*[a-z].*)";
-        String numbers = "(.*[0-9].*)";
-        String specialChars = "(.*[@,#,$,%].*$)";
 
-        if (password.length() < 8){
-            System.out.println("Passowrd must be at least 8 characters long.");
-            isValid = false;
-        }
-        if (!password.matches(upperCaseChars)){
-            System.out.println("Password must have at least 1 upper case character.");
-            isValid = false;
-        }
-        if (!password.matches(lowerCaseChars)){
-            System.out.println("Password must have at least 1 lower case character.");
-            isValid = false;
-        }
-        if (!password.matches(numbers)){
-            System.out.println("Password muse have at least 1 number.");
-            isValid = false;
-        }
-        if (!password.matches(specialChars)){
-            System.out.println("Password must have at least 1 special character.");
-            isValid = false;
-        }
-        if (isValid==true){
-            System.out.println("The password meets the requirements.");
-        }
-        return isValid;
+    public static boolean isValidPassword(String password) { //Checks if the password meets the specified requirements
+        String regex = "\\A(?=.*[0-9])(?=.*[a-z])(?=.*[@#$%^+=!])(?=\\S+$).{8,}\\z";
+        return password.matches(regex);
     }
 }
